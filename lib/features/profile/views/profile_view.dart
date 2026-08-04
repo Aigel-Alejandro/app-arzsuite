f2d9c006094e15fcfcbdb7caedb31a8fbe68deaa
Aigel-Alejandro <alejandro@aigel.com.mx>
2026-08-03 22:23:58 -0600

    Acortar etiqueta a Actividades

--- lib/features/profile/views/profile_view.dart
+++ lib/features/profile/views/profile_view.dart
@@ -1875,7 +1875,7 @@ class _ProfileViewState extends ConsumerState<ProfileView> {
                 */
                 _buildPermissionChip(
                   context,
-                  label: 'Inscribir Actividades',
+                  label: 'Actividades',
                   icon: Icons.sports_tennis_rounded,
                   isGranted: member.permissions.contains('activities.enroll'),
                   onChanged: (val) async {
